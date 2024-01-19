5258b826b359801ba39601ce843fcf6d20cf3897
kobi400 <iamstevebikomabuka@gmail.com>
2024-01-19 23:46:37 +0300

    This is ,y first repository, open to all contradictions with open recommendations for version committing.

---
+++ screens/homescreen.py
@@ -0,0 +1,10 @@
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import MDScreenManager
+
+
+class ScreenManager(MDScreenManager):
+    pass
+
+
+class HomeScreen(MDScreen):
+    pass
